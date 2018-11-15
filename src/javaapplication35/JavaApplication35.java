7ca3bcdcbf3359bd36d197fc2393e0e4c7294f91
niko <niko@172.19.9.175>
2018-11-15 10:22:13 +0100

    Prova

--- src/javaapplication35/JavaApplication35.java
+++ src/javaapplication35/JavaApplication35.java
@@ -17,6 +17,7 @@ public class JavaApplication35 {
     public static void main(String[] args) {
         // TODO code application logic here
         int b = 10;
+        int c = 20;
     }
     
 }
