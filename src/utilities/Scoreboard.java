6ed18b49b467f426669dcb6ac83ddfdb431c1dad
niko <niko@geforece>
2018-11-26 17:02:38 +0100

    Merge origin/master
    Conflicts:
    	src/javaapplication35/JavaApplication35.java

---
+++ src/utilities/Scoreboard.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author niko and angelo
+ */
+public class Scoreboard {
+    private final String FILENAME = "../resources/scores.txt";
+    private final int SIZE = 10;
+    private LinkedList<Score> scoreboard;
+    
+    
+    public Scoreboard(){
+        this.scoreboard = new LinkedList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))){
+            String line = null;
+            
+            while((line = br.readLine()) != null){
+                String[] tmp= line.split(" ");
+                scoreboard.add(new Score(tmp[0], Integer.parseInt(tmp[1])));
+            }
+        }
+        catch(IOException exc){exc.printStackTrace();}
+    }
+    
+    public List<Score> getScoreboard(){
+        return this.scoreboard;
+    }
+    
+    public void addScore(String player, int score){
+        if (this.SIZE < this.scoreboard.size()){}
+        
+        else if (score <= this.scoreboard.getLast().getScore())
+            return;
+        
+        else {this.scoreboard.removeLast();}
+        
+        int index = this.findGT(score);
+        this.scoreboard.add(index, new Score(player, score));
+        
+        this.saveScoreboard();
+    }
+    
+    public void saveScoreboard(){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME))){
+            for (Score item: this.scoreboard){
+                bw.write(item.toString());
+                bw.newLine();
+                bw.flush();
+            }
+        }
+        catch(IOException exc){exc.printStackTrace();}
+    }
+    
+    private int findGT(int score){
+        for (Score item: this.scoreboard){
+            if (score >= item.getScore())
+                return this.scoreboard.indexOf(item);
+        }
+        return -1;
+    }
+    
+    
+   private class Score{
+    private int score;
+    private String player;
+    
+    public Score(String player, int score){
+        this.score = score;
+        this.player = player;
+    }
+ 
+    public String getPlayer(){
+        return this.player;
+    }    
+    
+    public int getScore(){
+        return this.score;
+    }
+
+    @Override
+    public String toString(){
+        return this.getPlayer() + " " + Integer.toString(this.getScore());
+    }
+   } 
+}
